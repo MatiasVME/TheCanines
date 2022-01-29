492c7876d9b8ccfaa9f9aa6754e8d76200d9803c
Crirocoro <98650582+Crirocoro@users.noreply.github.com>
2022-01-29 15:43:48 -0300

    Re-estructuración de Código
    se ha agregado, un finite state machine, se consideró cambiar el diseño de estructuración, ahora la animación de salto, se produce mejor (jumpUp, jumpDown)

--- Game/Benny/Benny.gd
+++ Game/Benny/Benny.gd
@@ -1,42 +1,79 @@
 extends KinematicBody2D
 
-var SPEED := 300.0
+export (int) var run_speed = 225
+export (int) var jump_speed = -425
+export (int) var gravity = 750
 
-var dir := Vector2.ZERO
-var move := Vector2.ZERO
+enum {IDLE, WALK, JUMP, DEAD}
+var state
+var anim
+var new_anim
 
-var is_jumping := false
+var velocity = Vector2()
 
-func _process(delta):
-	move.y += 10
+func _ready():
+	change_state(IDLE)
+	pass
 	
-	dir.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+func change_state(new_state):
+	state = new_state
+	match state:
+		IDLE:
+			new_anim = 'idle'
+		WALK:
+			new_anim = 'walk'
+		JUMP:
+			new_anim = 'jumpUp'
+		DEAD:
+			pass
+	pass
+
+func start(pos):
+	position = pos
+	show()
+	change_state(IDLE)
+	pass
 	
-	if dir.x == -1:
-		$Sprite.flip_h = true
-		
-		if $Sprite.animation == "Idle":
-			$Sprite.play("Walk")
-	elif dir.x == 1:
+func get_input():
+	#Inputs
+	var right = Input.is_action_pressed("ui_right")
+	var left = Input.is_action_pressed("ui_left")
+	var jump = Input.is_action_just_pressed("ui_up")
+	
+	#Movement
+	velocity.x = 0
+	if right:
+		velocity.x += run_speed
 		$Sprite.flip_h = false
-		
-		if $Sprite.animation == "Idle":
-			$Sprite.play("Walk")
-	else:
-		$Sprite.play("Idle")
+	if left:
+		velocity.x -= run_speed
+		$Sprite.flip_h = true
+	#Jump
+	if jump and is_on_floor():
+		change_state(JUMP)
+		velocity.y = jump_speed
+	# idle transition to run
+	if state == IDLE and velocity.x != 0:
+		change_state(WALK)
+	# run transition to idle
+	if state == WALK and velocity.x == 0:
+		change_state(IDLE)
+	# transition to jump when falling
+	if state in [IDLE, WALK] and !is_on_floor():
+		change_state(JUMP)
+	pass
 	
-	move.x = dir.x * 200
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	get_input()
+	if new_anim != anim:
+		anim = new_anim
+		$Sprite.play(anim)
+	# move the player
+	velocity = move_and_slide(velocity, Vector2(0, -1))
 	
-	if is_on_floor():
-		move.y = 0
-		
-		if Input.is_action_just_pressed("ui_up"):
-			move.y = -400
-			$Sprite.play("JumpUp")
-	else:
-		if move.y > 0:
-			$Sprite.play("JumpDown")
-	
-	
-	
-	move_and_slide(move, Vector2.UP)
+	if state == JUMP and is_on_floor():
+		change_state(IDLE)
+	if state == JUMP and velocity.y > 0:
+		new_anim = 'jumpDown'
+	pass
