47756a3a492c663427a440cfec02deec2a0cc582
Matías Valentín Muñoz Espinoza <writkas2@gmail.com.com>
2022-01-29 11:14:38 -0300

    initial commit

---
+++ Transition/Transition.gd
@@ -0,0 +1,29 @@
+extends ColorRect
+
+var shader_pos
+
+
+func _process(delta):
+#	amount -= delta
+	material.set_shader_param("position", shader_pos)
+#	print(amount)
+
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		start()
+
+
+func start():
+	$Tween.interpolate_property(
+		self,
+		"shader_pos",
+		1,
+		-1.5, 
+		10,
+		Tween.TRANS_SINE,
+		Tween.EASE_IN_OUT
+	)
+	$Tween.start()
+	
+
