476b712ba0178da1e5d65d6eb12da644463ed972
Matías Valentín Muñoz Espinoza <writkas2@gmail.com.com>
2022-01-29 22:45:36 -0300

    update

--- Main.gd
+++ Main.gd
@@ -3,4 +3,4 @@ extends Node
 
 func _ready():
 	MusicManager.play(MusicManager.Music.MENU_SCREEN)
-	Transition.change_scene("res://MainScreen/Menu/MenuScreen.tscn")
+	get_tree().change_scene("res://MainScreen/Menu/MenuScreen.tscn")
