3d4425815323f36b37ca9fa4e2de0dd0eae22cd9
Matías Valentín Muñoz Espinoza <writkas2@gmail.com.com>
2022-01-30 02:37:55 -0300

    updatee

--- Game/Rooms/Basement/Basement.gd
+++ Game/Rooms/Basement/Basement.gd
@@ -3,3 +3,8 @@ extends Node2D
 
 func _ready():
 	$Benny.set_cam_limit($CameraPoints/Pos1.global_position, $CameraPoints/Pos2.global_position)
+
+
+func _on_Exit_area_entered(area):
+#	get_tree().change_scene()
+	pass
